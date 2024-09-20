01f14c2b8b4f323885402ee275decb88e4a9143e
mubashir-edk <mubashiredk03@gmail.com>
2024-09-20 16:34:26 +0530

    fixed callbacks issue

--- functions/train.py
+++ functions/train.py
@@ -111,45 +111,38 @@ def create_generators(train_df, valid_df, test_df, batch_size):
     
     return train_gen, valid_gen, test_gen
     
-class MyCallback(keras.callbacks.Callback):
+class MyCallback(tf.keras.callbacks.Callback):
     def __init__(self, model, patience, stop_patience, threshold, factor, batches, epochs, ask_epoch):
         super(MyCallback, self).__init__()
-        self.model = model
-        # specifies how many epochs without improvement before learning rate is adjusted
+        self._model = model  # Use a private attribute to store the model
         self.patience = patience
-        # specifies how many times to adjust lr without improvement to stop training
         self.stop_patience = stop_patience
-        # specifies training accuracy threshold when lr will be adjusted based on validation loss
         self.threshold = threshold
         self.factor = factor  # factor by which to reduce the learning rate
-        self.batches = batches  # number of training batch to run per epoch
+        self.batches = batches  # number of training batches to run per epoch
         self.epochs = epochs
         self.ask_epoch = ask_epoch
-        # save this value to restore if restarting training
         self.ask_epoch_initial = ask_epoch
 
         # callback variables
         self.count = 0  # how many times lr has been reduced without improvement
         self.stop_count = 0
         self.best_epoch = 1   # epoch with the lowest loss
-        # get the initial learning rate and save it
-        self.initial_lr = float(tf.keras.backend.get_value(model.optimizer.lr))
-        self.highest_tracc = 0.0  # set highest training accuracy to 0 initially
-        self.lowest_vloss = np.inf  # set lowest validation loss to infinity initially
-        # set best weights to model's initial weights
-        self.best_weights = self.model.get_weights()
-        # save initial weights if they have to get restored
-        self.initial_weights = self.model.get_weights()
+        self.initial_lr = float(tf.keras.backend.get_value(self._model.optimizer.learning_rate))
+        self.highest_tracc = 0.0
+        self.lowest_vloss = np.inf
+        self.best_weights = self._model.get_weights()
+        self.initial_weights = self._model.get_weights()
 
-    # Define a function that will run when train begins
+    @property
+    def model(self):
+        return self._model
+
     def on_train_begin(self, logs=None):
-        msg = 'Do you want model asks you to halt the training [y/n] ?'
+        msg = 'Do you want model asks you to halt the training [y/n]?'
         print(msg)
         ans = input('')
-        if ans in ['Y', 'y']:
-            self.ask_permission = 1
-        elif ans in ['N', 'n']:
-            self.ask_permission = 0
+        self.ask_permission = 1 if ans in ['Y', 'y'] else 0
 
         msg = '{0:^8s}{1:^10s}{2:^9s}{3:^9s}{4:^9s}{5:^9s}{6:^9s}{7:^10s}{8:10s}{9:^8s}'.format(
             'Epoch', 'Loss', 'Accuracy', 'V_loss', 'V_acc', 'LR', 'Next LR', 'Monitor', '% Improv', 'Duration')
@@ -163,139 +156,111 @@ class MyCallback(keras.callbacks.Callback):
         minutes = (tr_duration - (hours * 3600)) // 60
         seconds = tr_duration - ((hours * 3600) + (minutes * 60))
 
-        msg = f'training elapsed time was {str(hours)} hours, {minutes:4.1f} minutes, {seconds:4.2f} seconds)'
+        msg = f'Training elapsed time was {str(hours)} hours, {minutes:4.1f} minutes, {seconds:4.2f} seconds)'
         print(msg)
 
         # set the weights of the model to the best weights
         self.model.set_weights(self.best_weights)
 
     def on_train_batch_end(self, batch, logs=None):
-        # get batch accuracy and loss
         acc = logs.get('accuracy') * 100
         loss = logs.get('loss')
 
-        # prints over on the same line to show running batch count
-        msg = '{0:20s}processing batch {1:} of {2:5s}-   accuracy=  {3:5.3f}   -   loss: {4:8.5f}'.format(
+        msg = '{0:20s} Processing batch {1:} of {2:5s} - Accuracy: {3:5.3f} - Loss: {4:8.5f}'.format(
             ' ', str(batch), str(self.batches), acc, loss)
         print(msg, '\r', end='')
 
     def on_epoch_begin(self, epoch, logs=None):
         self.ep_start = time.time()
 
-    # Define method runs on the end of each epoch
-
     def on_epoch_end(self, epoch, logs=None):
         ep_end = time.time()
         duration = ep_end - self.ep_start
 
-        # get the current learning rate
         lr = float(tf.keras.backend.get_value(self.model.optimizer.lr))
-        current_lr = lr
-        acc = logs.get('accuracy')  # get training accuracy
-        v_acc = logs.get('val_accuracy')  # get validation accuracy
-        loss = logs.get('loss')  # get training loss for this epoch
-        v_loss = logs.get('val_loss')  # get the validation loss for this epoch
+        acc = logs.get('accuracy')
+        v_acc = logs.get('val_accuracy')
+        loss = logs.get('loss')
+        v_loss = logs.get('val_loss')
 
         if acc < self.threshold:  # if training accuracy is below threshold adjust lr based on training accuracy
             monitor = 'accuracy'
             if epoch == 0:
                 pimprov = 0.0
             else:
-                # define improvement of model progres
                 pimprov = (acc - self.highest_tracc) * 100 / self.highest_tracc
 
-            if acc > self.highest_tracc:  # training accuracy improved in the epoch
-                self.highest_tracc = acc  # set new highest training accuracy
-                # training accuracy improved so save the weights
+            if acc > self.highest_tracc:  # training accuracy improved
+                self.highest_tracc = acc
                 self.best_weights = self.model.get_weights()
-                self.count = 0  # set count to 0 since training accuracy improved
-                self.stop_count = 0  # set stop counter to 0
+                self.count = 0
+                self.stop_count = 0
                 if v_loss < self.lowest_vloss:
                     self.lowest_vloss = v_loss
-                self.best_epoch = epoch + 1  # set the value of best epoch for this epoch
+                self.best_epoch = epoch + 1
 
             else:
-                # training accuracy did not improve check if this has happened for patience number of epochs
-                # if so adjust learning rate
-                if self.count >= self.patience - 1:  # lr should be adjusted
-                    lr = lr * self.factor  # adjust the learning by factor
-                    # set the learning rate in the optimizer
+                if self.count >= self.patience - 1:  # adjust learning rate
+                    lr = lr * self.factor
                     tf.keras.backend.set_value(self.model.optimizer.lr, lr)
-                    self.count = 0  # reset the count to 0
-                    # count the number of consecutive lr adjustments
-                    self.stop_count = self.stop_count + 1
-                    self.count = 0  # reset counter
-                    if v_loss < self.lowest_vloss:
-                        self.lowest_vloss = v_loss
+                    self.count = 0
+                    self.stop_count += 1
                 else:
-                    self.count = self.count + 1  # increment patience counter
+                    self.count += 1
 
-        else:  # training accuracy is above threshold so adjust learning rate based on validation loss
+        else:  # training accuracy is above threshold
             monitor = 'val_loss'
             if epoch == 0:
                 pimprov = 0.0
-
             else:
-                pimprov = (self.lowest_vloss - v_loss) * \
-                    100 / self.lowest_vloss
+                pimprov = (self.lowest_vloss - v_loss) * 100 / self.lowest_vloss
 
-            if v_loss < self.lowest_vloss:  # check if the validation loss improved
-                self.lowest_vloss = v_loss  # replace lowest validation loss with new validation loss
-                # validation loss improved so save the weights
+            if v_loss < self.lowest_vloss:  # validation loss improved
+                self.lowest_vloss = v_loss
                 self.best_weights = self.model.get_weights()
-                self.count = 0  # reset count since validation loss improved
+                self.count = 0
                 self.stop_count = 0
-                self.best_epoch = epoch + 1  # set the value of the best epoch to this epoch
-
-            else:  # validation loss did not improve
-                if self.count >= self.patience - 1:  # need to adjust lr
-                    lr = lr * self.factor  # adjust the learning rate
-                    # increment stop counter because lr was adjusted
-                    self.stop_count = self.stop_count + 1
-                    self.count = 0  # reset counter
-                    # set the learning rate in the optimizer
+                self.best_epoch = epoch + 1
+            else:
+                if self.count >= self.patience - 1:
+                    lr = lr * self.factor
                     tf.keras.backend.set_value(self.model.optimizer.lr, lr)
-
+                    self.stop_count += 1
+                    self.count = 0
                 else:
-                    self.count = self.count + 1  # increment the patience counter
+                    self.count += 1
 
                 if acc > self.highest_tracc:
                     self.highest_tracc = acc
 
-        msg = f'{str(epoch + 1):^3s}/{str(self.epochs):4s} {loss:^9.3f}{acc * 100:^9.3f}{v_loss:^9.5f}{v_acc * 100:^9.3f}{current_lr:^9.5f}{lr:^9.5f}{monitor:^11s}{pimprov:^10.2f}{duration:^8.2f}'
+        msg = f'{str(epoch + 1):^3s}/{str(self.epochs):4s} {loss:^9.3f}{acc * 100:^9.3f}{v_loss:^9.5f}{v_acc * 100:^9.3f}{lr:^9.5f}{monitor:^11s}{pimprov:^10.2f}{duration:^8.2f}'
         print(msg)
 
-        # check if learning rate has been adjusted stop_count times with no improvement
         if self.stop_count > self.stop_patience - 1:
-            msg = f' training has been halted at epoch {epoch + 1} after {self.stop_patience} adjustments of learning rate with no improvement'
+            msg = f'Training has been halted at epoch {epoch + 1} after {self.stop_patience} adjustments of learning rate with no improvement.'
             print(msg)
-            self.model.stop_training = True  # stop training
+            self.model.stop_training = True
 
-        else:
-            if self.ask_epoch != None and self.ask_permission != 0:
-                if epoch + 1 >= self.ask_epoch:
-                    msg = 'enter H to halt training or an integer for number of epochs to run then ask again'
+        if self.ask_epoch is not None and self.ask_permission != 0:
+            if epoch + 1 >= self.ask_epoch:
+                msg = 'Enter H to halt training or an integer for number of epochs to run then ask again:'
+                print(msg)
+
+                ans = input('')
+                if ans in ['H', 'h']:
+                    msg = f'Training has been halted at epoch {epoch + 1} due to user input.'
                     print(msg)
-
-                    ans = input('')
-                    if ans == 'H' or ans == 'h':
-                        msg = f'training has been halted at epoch {epoch + 1} due to user input'
+                    self.model.stop_training = True
+                else:
+                    try:
+                        ans = int(ans)
+                        self.ask_epoch += ans
+                        msg = f'Training will continue until epoch {str(self.ask_epoch)}.'
                         print(msg)
-                        self.model.stop_training = True  # stop training
-
-                    else:
-                        try:
-                            ans = int(ans)
-                            self.ask_epoch += ans
-                            msg = f' training will continue until epoch {str(self.ask_epoch)}'
-                            print(msg)
-                            msg = '{0:^8s}{1:^10s}{2:^9s}{3:^9s}{4:^9s}{5:^9s}{6:^9s}{7:^10s}{8:10s}{9:^8s}'.format(
-                                'Epoch', 'Loss', 'Accuracy', 'V_loss', 'V_acc', 'LR', 'Next LR', 'Monitor', '% Improv', 'Duration')
-                            print(msg)
-
-                        except Exception:
-                            print('Invalid')
-
+                        print('{0:^8s}{1:^10s}{2:^9s}{3:^9s}{4:^9s}{5:^9s}{6:^9s}{7:^10s}{8:10s}{9:^8s}'.format(
+                            'Epoch', 'Loss', 'Accuracy', 'V_loss', 'V_acc', 'LR', 'Next LR', 'Monitor', '% Improv', 'Duration'))
+                    except Exception:
+                        print('Invalid input.')
 
 def training_model(test_df, train_gen, valid_gen, test_gen, batch_size, steps_per_epoch, epochs, patience, stop_patience, threshold, factor, ask_epoch):
     
@@ -323,14 +288,14 @@ def training_model(test_df, train_gen, valid_gen, test_gen, batch_size, steps_pe
     
     # test and evaluate
     ts_length = len(test_df)
-    test_batch_size = test_batch_size = max(sorted([ts_length // n for n in range(1, ts_length + 1) if ts_length % n == 0 and ts_length/n <= 80]))
+    test_batch_size = max(sorted([ts_length // n for n in range(1, ts_length + 1) if ts_length % n == 0 and ts_length/n <= 80]))
     test_steps = ts_length // test_batch_size
     
     train_score = model.evaluate(train_gen, steps=test_steps, verbose=1)
     valid_score = model.evaluate(valid_gen, steps=test_steps, verbose=1)
     test_score = model.evaluate(test_gen, steps=test_steps, verbose=1)
-    
-    return model, train_score, valid_score, test_score
+
+    return model, history, train_score, valid_score, test_score
 
 
 def save_model(model, test_score):
