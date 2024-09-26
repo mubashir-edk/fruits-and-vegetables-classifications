0a6094334c86ede9af47b57a1e599be49c809d86
mubashir-edk <mubashiredk03@gmail.com>
2024-09-26 18:15:21 +0530

    removed custom callbacks

--- functions/train.py
+++ functions/train.py
@@ -10,8 +10,9 @@ import keras
 import time
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
-from tensorflow.keras import regularizers
-from tensorflow.keras.optimizers import Adam, Adamax
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+from tensorflow.keras.losses import CategoricalCrossentropy
 
 router = APIRouter()
 
@@ -110,181 +111,39 @@ def create_generators(train_df, valid_df, test_df, batch_size):
     )
     
     return train_gen, valid_gen, test_gen
-    
-class MyCallback(tf.keras.callbacks.Callback):
-    def __init__(self, model, patience, stop_patience, threshold, factor, batches, epochs, ask_epoch):
-        super(MyCallback, self).__init__()
-        self._model = model  # Use a private attribute to store the model
-        self.patience = patience
-        self.stop_patience = stop_patience
-        self.threshold = threshold
-        self.factor = factor  # factor by which to reduce the learning rate
-        self.batches = batches  # number of training batches to run per epoch
-        self.epochs = epochs
-        self.ask_epoch = ask_epoch
-        self.ask_epoch_initial = ask_epoch
 
-        # callback variables
-        self.count = 0  # how many times lr has been reduced without improvement
-        self.stop_count = 0
-        self.best_epoch = 1   # epoch with the lowest loss
-        self.initial_lr = float(tf.keras.backend.get_value(self._model.optimizer.learning_rate))
-        self.highest_tracc = 0.0
-        self.lowest_vloss = np.inf
-        self.best_weights = self._model.get_weights()
-        self.initial_weights = self._model.get_weights()
-
-    @property
-    def model(self):
-        return self._model
-
-    def on_train_begin(self, logs=None):
-        msg = 'Do you want model asks you to halt the training [y/n]?'
-        print(msg)
-        ans = input('')
-        self.ask_permission = 1 if ans in ['Y', 'y'] else 0
-
-        msg = '{0:^8s}{1:^10s}{2:^9s}{3:^9s}{4:^9s}{5:^9s}{6:^9s}{7:^10s}{8:10s}{9:^8s}'.format(
-            'Epoch', 'Loss', 'Accuracy', 'V_loss', 'V_acc', 'LR', 'Next LR', 'Monitor', '% Improv', 'Duration')
-        print(msg)
-        self.start_time = time.time()
-
-    def on_train_end(self, logs=None):
-        stop_time = time.time()
-        tr_duration = stop_time - self.start_time
-        hours = tr_duration // 3600
-        minutes = (tr_duration - (hours * 3600)) // 60
-        seconds = tr_duration - ((hours * 3600) + (minutes * 60))
-
-        msg = f'Training elapsed time was {str(hours)} hours, {minutes:4.1f} minutes, {seconds:4.2f} seconds)'
-        print(msg)
-
-        # set the weights of the model to the best weights
-        self.model.set_weights(self.best_weights)
-
-    def on_train_batch_end(self, batch, logs=None):
-        acc = logs.get('accuracy') * 100
-        loss = logs.get('loss')
-
-        msg = '{0:20s} Processing batch {1:} of {2:5s} - Accuracy: {3:5.3f} - Loss: {4:8.5f}'.format(
-            ' ', str(batch), str(self.batches), acc, loss)
-        print(msg, '\r', end='')
-
-    def on_epoch_begin(self, epoch, logs=None):
-        self.ep_start = time.time()
-
-    def on_epoch_end(self, epoch, logs=None):
-        ep_end = time.time()
-        duration = ep_end - self.ep_start
-
-        lr = float(tf.keras.backend.get_value(self.model.optimizer.lr))
-        acc = logs.get('accuracy')
-        v_acc = logs.get('val_accuracy')
-        loss = logs.get('loss')
-        v_loss = logs.get('val_loss')
-
-        if acc < self.threshold:  # if training accuracy is below threshold adjust lr based on training accuracy
-            monitor = 'accuracy'
-            if epoch == 0:
-                pimprov = 0.0
-            else:
-                pimprov = (acc - self.highest_tracc) * 100 / self.highest_tracc
-
-            if acc > self.highest_tracc:  # training accuracy improved
-                self.highest_tracc = acc
-                self.best_weights = self.model.get_weights()
-                self.count = 0
-                self.stop_count = 0
-                if v_loss < self.lowest_vloss:
-                    self.lowest_vloss = v_loss
-                self.best_epoch = epoch + 1
-
-            else:
-                if self.count >= self.patience - 1:  # adjust learning rate
-                    lr = lr * self.factor
-                    tf.keras.backend.set_value(self.model.optimizer.lr, lr)
-                    self.count = 0
-                    self.stop_count += 1
-                else:
-                    self.count += 1
-
-        else:  # training accuracy is above threshold
-            monitor = 'val_loss'
-            if epoch == 0:
-                pimprov = 0.0
-            else:
-                pimprov = (self.lowest_vloss - v_loss) * 100 / self.lowest_vloss
-
-            if v_loss < self.lowest_vloss:  # validation loss improved
-                self.lowest_vloss = v_loss
-                self.best_weights = self.model.get_weights()
-                self.count = 0
-                self.stop_count = 0
-                self.best_epoch = epoch + 1
-            else:
-                if self.count >= self.patience - 1:
-                    lr = lr * self.factor
-                    tf.keras.backend.set_value(self.model.optimizer.lr, lr)
-                    self.stop_count += 1
-                    self.count = 0
-                else:
-                    self.count += 1
-
-                if acc > self.highest_tracc:
-                    self.highest_tracc = acc
-
-        msg = f'{str(epoch + 1):^3s}/{str(self.epochs):4s} {loss:^9.3f}{acc * 100:^9.3f}{v_loss:^9.5f}{v_acc * 100:^9.3f}{lr:^9.5f}{monitor:^11s}{pimprov:^10.2f}{duration:^8.2f}'
-        print(msg)
-
-        if self.stop_count > self.stop_patience - 1:
-            msg = f'Training has been halted at epoch {epoch + 1} after {self.stop_patience} adjustments of learning rate with no improvement.'
-            print(msg)
-            self.model.stop_training = True
-
-        if self.ask_epoch is not None and self.ask_permission != 0:
-            if epoch + 1 >= self.ask_epoch:
-                msg = 'Enter H to halt training or an integer for number of epochs to run then ask again:'
-                print(msg)
-
-                ans = input('')
-                if ans in ['H', 'h']:
-                    msg = f'Training has been halted at epoch {epoch + 1} due to user input.'
-                    print(msg)
-                    self.model.stop_training = True
-                else:
-                    try:
-                        ans = int(ans)
-                        self.ask_epoch += ans
-                        msg = f'Training will continue until epoch {str(self.ask_epoch)}.'
-                        print(msg)
-                        print('{0:^8s}{1:^10s}{2:^9s}{3:^9s}{4:^9s}{5:^9s}{6:^9s}{7:^10s}{8:10s}{9:^8s}'.format(
-                            'Epoch', 'Loss', 'Accuracy', 'V_loss', 'V_acc', 'LR', 'Next LR', 'Monitor', '% Improv', 'Duration'))
-                    except Exception:
-                        print('Invalid input.')
-
-def training_model(test_df, train_gen, valid_gen, test_gen, batch_size, steps_per_epoch, epochs, patience, stop_patience, threshold, factor, ask_epoch):
+def training_model(test_df, train_gen, valid_gen, test_gen, batch_size, steps_per_epoch, epochs):
     
     img_size = (224, 224)
     channels = 3
     img_shape = (img_size[0], img_size[1], channels)
     
-    base_model = tf.keras.applications.MobileNetV2(include_top=False, weights="imagenet", input_shape=img_shape, pooling='max')
+    base_model = tf.keras.applications.MobileNetV2(input_shape=img_shape, include_top=False, weights="imagenet", pooling='avg')
     model = Sequential([
         base_model,
-        BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001),
         Dense(256, activation='relu'),
-        Dropout(rate=0.45, seed=123),
+        Dense(128, activation='relu'),
+        Dropout(0.4),
         Dense(4, activation='softmax')
     ])
 
-    model.compile(Adamax(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=Adam(learning_rate=1e-4), loss=CategoricalCrossentropy(), metrics=['accuracy'])
     model.summary()
     
-    batches = int(np.ceil(len(train_gen.labels) / batch_size))
+    train_gen = train_gen
     
-    callbacks = [MyCallback(model=model, patience=patience, stop_patience=stop_patience, threshold=threshold, factor=factor, batches=batches, epochs=epochs, ask_epoch=ask_epoch)]
+    # Model CheckPoint
+    checkpoint_cb = ModelCheckpoint('/trained_model/MyModel.keras', monitor='val_accuracy', save_best_only=True)
+
+    # Early Stoping
+    earlystop_cb = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+
+    # ReduceLROnPlateau
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.3, patience=3, min_lr=1e-6)
     
-    history = model.fit(train_gen, batch_size=batch_size, steps_per_epoch=steps_per_epoch, epochs=epochs, verbose=0, validation_data=valid_gen, validation_steps=None, shuffle=False)
+    callbacks = [checkpoint_cb, earlystop_cb, reduce_lr]
+    
+    history = model.fit(train_gen, batch_size=batch_size, steps_per_epoch=steps_per_epoch, callbacks=callbacks,epochs=epochs, verbose=0, validation_data=valid_gen, validation_steps=15, shuffle=False)
     
     # test and evaluate
     ts_length = len(test_df)
@@ -295,14 +154,16 @@ def training_model(test_df, train_gen, valid_gen, test_gen, batch_size, steps_pe
     valid_score = model.evaluate(valid_gen, steps=test_steps, verbose=1)
     test_score = model.evaluate(test_gen, steps=test_steps, verbose=1)
 
-    return model, history, train_score, valid_score, test_score
+    return model, train_score, valid_score, test_score
 
 
-def save_model(model, test_score):
+def save_model(model):
 
     subject = 'fruits_and_vegetables_classification'
-    acc = test_score[1] * 100
-    save_path = '/trained_model'
+    current_dir = os.path.dirname(__file__)
+    save_path = os.path.join(current_dir, '..', 'trained_model')
+    save_path = os.path.abspath(save_path)
+    os.makedirs(save_path, exist_ok=True)
 
     # Save model
     save_id = str(f'{subject}.h5')
--- routers/train_model.py
+++ routers/train_model.py
@@ -23,27 +23,17 @@ async def train_model():
     
     # Step 3 Split the data(dataframe) in to train, validation and test
     train_df, valid_df, test_df = split_data(data_frame)
-    
-    # Calculating batch size
-    # total_samples = train_df.samples
-    # num_batches = len(train_df)
-    # print(total_samples, num_batches)
-    # batch_size = np.ceil(total_samples / num_batches)
-    batch_size = 32
+
+    batch_size = 16
     
     # Step 4 create generators
     train_gen, valid_gen, test_gen = create_generators(train_df, valid_df, test_df, batch_size)
     
     steps_per_epoch = 20
-    epochs = 100
-    patience = 3
-    stop_patience = 5
-    threshold = 0.9
-    factor = 0.5
-    ask_epoch = 4
+    epochs = 120
     
     # Step 5 Train the model and after that test and evaluate
-    model, train_score, valid_score, test_score = training_model(test_df, train_gen, valid_gen, test_gen, batch_size, steps_per_epoch, epochs, patience, stop_patience, threshold, factor, ask_epoch)
+    model, train_score, valid_score, test_score = training_model(test_df, train_gen, valid_gen, test_gen, batch_size, steps_per_epoch, epochs)
     
     print("Train Loss: ", train_score[0])
     print("Train Accuracy: ", train_score[1])
@@ -55,7 +45,7 @@ async def train_model():
     print("Test Accuracy: ", test_score[1])
     
     # Step 6 Save the trained model and its model weights
-    save_model(model, test_score)
+    save_model(model)
     
     return train_score, valid_score, test_score
     
